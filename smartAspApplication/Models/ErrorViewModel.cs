e80b156a5b7d48f4c2df6b7e2e786e3a0e4ed019
Mik pc git <imtiazkhan607@gmail.com>
2026-07-08 03:40:34 +0500

    Add project files.

---
+++ smartAspApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace smartAspApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
